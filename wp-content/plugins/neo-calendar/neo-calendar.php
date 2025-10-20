3c2ab81a1520b4c1d224c9e0389bc49ca917ad5b
MrFreuden <mrfreuden@gmail.com>
2025-10-20 15:32:46 +0200

    test

--- wp-content/plugins/neo-calendar/neo-calendar.php
+++ wp-content/plugins/neo-calendar/neo-calendar.php
@@ -141,7 +141,7 @@ add_action('plugins_loaded', static function () {
         // Erstelle Gruppe in Sidebar
         do_action('neo_dashboard_register_sidebar_item', [
             'slug'     => 'neo-calendar-group',
-            'label'    => 'Neo Calendar TEST',
+            'label'    => 'Neo Calendar TEST123',
             'icon'     => 'bi-calendar-event',
             'url'      => '/neo-dashboard/neo-calendar',
             'position' => 25,
